2b138d7e898b587a0213aef52db307057b089d30
Abdullah-i5o <abdullah@ifiveo.com>
2023-08-08 14:17:53 +0500

    update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import glob
 import os
-from PIL import Image
 
-# import cv2
+
+import cv2
 import streamlit as st
 
+
 st.title('Demo Streamlit Share')
-files = glob.glob(os.getcwd() + '/*.jpg')
 
-source = st.selectbox('Select a file for display', tuple(files))
+files = glob.glob(os.getcwd() + '/*.png')
+file_name = tuple(os.path.basename(i) for i in files)
+print(file_name)
+source = st.selectbox('Select a file for display', tuple(file_name))
+source_path = os.path.join(os.getcwd(), source)
 
-image = Image.open(source)
+image = cv2.imread(source_path)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
 st.image(image, caption="Output Image")
